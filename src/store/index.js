0afb48ac79a605a1c0b00891ce67b3dd8f221734
Andrey <skrtroot@protonmail.com>
2018-01-16 16:53:25 +0300

    add redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,18 @@
 // @flow
 import { createStore, applyMiddleware } from "redux";
-import xstreamAdapter from 'redux-observable-adapter-xstream';
-import { createEpicMiddleware } from 'redux-observable';
+import xstreamAdapter from "redux-observable-adapter-xstream";
+import { createEpicMiddleware } from "redux-observable";
 import reducer from "./reducers";
 import epic from "./epics";
 
 const epicMiddleware = createEpicMiddleware(epic, { adapter: xstreamAdapter });
+const middlewares = [epicMiddleware];
 
-const store = createStore(reducer, applyMiddleware(epicMiddleware));
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+
+  middlewares.push(logger);
+}
+
+const store = createStore(reducer, applyMiddleware(...middlewares));
 export default store;
