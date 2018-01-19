3018bb35e12e89e667a694cdab5fd9688dcb4390
Andrey <skrtroot@protonmail.com>
2018-01-19 21:40:28 +0300

    Add more items to trains

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ const ReminderToggle = ({ state, onClick = () => {} }) => (
   </RoundedElipsis>
 );
 
-const trains = ["Baker st. 15:00", "Vasilevskaya st. 20:00"];
+const trains = ["Baker st. 15:00", "Vostochnaya st. 03:30", "Vasilevskaya st. 20:00", "Mayakovskaya st. 19:00", "Shilo st. 21:00", "Aleksandrovskaya st. 22:00", "Krasnaya ploshad st. 05:00", "Gruzinskaya st. 12:00", "Baker st. 15:00", "Vostochnaya st. 03:30", "Vasilevskaya st. 20:00", "Mayakovskaya st. 19:00", "Shilo st. 21:00", "Aleksandrovskaya st. 22:00", "Krasnaya ploshad st. 05:00", "Gruzinskaya st. 12:00"];
 
 export class App extends Component {
   static defaultProps = {
