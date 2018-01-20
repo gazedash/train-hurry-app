4aa7d42eab745ce863980157980fa1e56c6995ab
Andrey <skrtroot@protonmail.com>
2018-01-20 09:31:40 +0300

    simple websocket server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var sockjs = require('sockjs');
+
+var echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js' });
+echo.on('connection', function(conn) {
+    conn.write('n' + JSON.stringify(Math.random()));
+    setInterval(function () {
+        conn.write('n' + JSON.stringify(Math.random()));
+    }, 12000);
+    // conn.on('data', function(message) {})
+    conn.on('close', function() {console.log('close')});
+});
+
+var server = http.createServer();
+echo.installHandlers(server, {prefix:'/echo'});
+server.listen(9999, '0.0.0.0');
